96ac009fa1f9d83829d60bccce00e26b4ab5f23b
Niels Reijnders <36259700+nielsreijnders@users.noreply.github.com>
2023-10-20 18:06:37 +0200

    feat(rollup): fix issues use client

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,49 +1,58 @@
+
 // rollup.config.js
 import terser from '@rollup/plugin-terser';
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
-import typescript from "@rollup/plugin-typescript";
-// import dts from "rollup-plugin-dts";
+import typescript from 'rollup-plugin-typescript2';
+import dts from "rollup-plugin-dts";
 import postcss from "rollup-plugin-postcss";
 import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 import multi from '@rollup/plugin-multi-entry';
+import preserveDirectives from 'rollup-plugin-preserve-directives';
 
 import pkg from "./package.json" assert { type: 'json' };
 
+
 export default [
 	{
-		input: {
-			// invite everyone!
-			include: ['src/components/**/*.tsx', 'src/functions/**/*.ts'],
-			// except for the joker
-			exclude: ['src/components/**/*.stories.tsx'],
-		},
+		input: 'src/index.ts',
 		output: [
 			{
-				file: pkg.main,
+				// exports: 'named',
+				// preserveModules: true,
+				// file: pkg.main,
+				preserveModules: true,
+				dir: 'dist/cjs', // Use 'dir' instead of 'file'
 				format: 'cjs',
 				sourcemap: true,
 			},
 			{
-				file: pkg.module,
+				// exports: 'named',
+				// preserveModules: true,
+				// file: pkg.module,
+				preserveModules: true,
+				dir: 'dist/esm', // Use 'dir' instead of 'file'
 				format: "esm",
 				sourcemap: true,
 			},
 		],
 		plugins: [
+			typescript({tsconfig: "./tsconfig.json", rootDir: "src" }),
+			multi(),
 			peerDepsExternal(),
 			resolve(),
-			multi(),
 			postcss({ modules: true }),
-			terser(),
+			preserveDirectives(),
+			terser({
+				compress: { directives: false },
+			}),
 			commonjs(),
-			typescript({ tsconfig: "./tsconfig.json" }),
 		]
 	},
-	// {
-	// 	input: "dist/esm/types/index.d.ts",
-	// 	output: [{ file: "dist/index.d.ts", format: "esm" }],
-	// 	plugins: [dts()],
-	// 	external: [/\.(css|less|scss)$/],
-	// },
+	{
+		input: "dist/esm/types/index.d.ts",
+		output: [{ file: "dist/index.d.ts", format: "esm" }],
+		plugins: [dts()],
+		external: [/\.(css|less|scss)$/],
+	},
 ];
