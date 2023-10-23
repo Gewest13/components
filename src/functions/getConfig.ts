c44df7a0f8f6e16ec1ad1a6bb0c12bf382cc2a6e
Niels Reijnders <36259700+nielsreijnders@users.noreply.github.com>
2023-10-23 11:02:01 +0200

    fix(getConfig): make root config path variable

--- src/functions/getConfig.ts
+++ src/functions/getConfig.ts
@@ -1,14 +1,18 @@
 /* eslint-disable @typescript-eslint/ban-ts-comment */
 // @ts-ignore
 import defaultData from '../../config';
+import path from 'path';
+
+const rootConfigPath = path.join(__dirname, 'config.js');
 
 let config;
 
 try {
-  config = require('../../../../config.js');
+  config = require(rootConfigPath);
+  console.log(config);
 } catch (error) {
   // Handle the error or provide default data
-  config = defaultData
+  config = defaultData;
 }
 
 export default config as any;
