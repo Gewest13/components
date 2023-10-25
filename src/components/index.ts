96f30455fb66f67578e576c4db2bd8d95b84f117
rubenvangrins <ruben@gewest13.nl>
2023-10-25 14:48:27 +0200

    feat(index): swiper component

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,4 @@
-// export { default as Input } from "./Input";
 export { Video, VideoComponent, FullVideo, enterFullScreen, exitFullScreen, ImperativeFullVideoRef } from "./Video/Video";
 export { Image } from "./Image/Image";
 export { ColumnsContainer } from "./ColumnsContainer/ColumnsContainer";
+export { Swiper, SwiperImperativeHandle } from "./Swiper/Swiper";
