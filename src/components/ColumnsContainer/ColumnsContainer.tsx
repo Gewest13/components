74c884b1d124e49d9fe016ea0421ab96807f6ede
Niels Reijnders <36259700+nielsreijnders@users.noreply.github.com>
2023-10-26 10:18:00 +0200

    feat(columnscontainer): remove default row

--- src/components/ColumnsContainer/ColumnsContainer.tsx
+++ src/components/ColumnsContainer/ColumnsContainer.tsx
@@ -1,10 +1,7 @@
 import React, { forwardRef } from 'react';
 
-import styles from './ColumnsContainer.module.scss';
 import { cssColumRowVars } from '../../functions/grid';
 import { Grid } from '../../interface';
-
-
 
 export interface IColumnsContainerProps {
   Container: React.ElementType
@@ -22,7 +19,7 @@ export const ColumnsContainer = forwardRef<HTMLDivElement, IColumnsContainerProp
     <Container ref={ref}>
       {columns.map((column, index) => {
         return (
-          <div style={cssColumRowVars(column.grids)} className={`${styles.gridItem} ${column.className || ''}`} data-grid={true} key={index}>
+          <div style={cssColumRowVars(column.grids)} className={`${column.className || ''}`} data-grid={true} key={index}>
             {column.component}
           </div>
         )
