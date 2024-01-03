99b5568d3cbba9efbc28f8adda15ebf6a00a8df9
Niels Reijnders <36259700+nielsreijnders@users.noreply.github.com>
2024-01-03 09:47:22 +0100

    Add better debug errors

--- src/components/SharedForm/SharedForm.tsx
+++ src/components/SharedForm/SharedForm.tsx
@@ -4,13 +4,8 @@ import { createContainer } from '../../functions/createContainer';
 import { EmailBody, wpMailResponse } from '../../functions/wpMail';
 import { RecaptchaV3, getToken } from '../RecaptchaV3/RecaptchaV3';
 
-export interface ISharedFormProps {
-  // ** Preferable a form element */
-  Container: React.ReactElement<HTMLDivElement> | React.ForwardRefExoticComponent<any | React.RefAttributes<HTMLDivElement>>
 
-  // ** The children of the form */
-  children?: React.ReactNode
-
+export interface ISharedFormWordpressData {
   // ** Confirmation subject */
   confirmationSubject?: string
   // ** Confirmation preview text */
@@ -35,7 +30,17 @@ export interface ISharedFormProps {
   mailTransport: {
     databaseId: (number | string)
   }
+  // ** The sender envelope */
   senderEnvelope: string
+}
+
+export interface ISharedFormProps {
+  // ** Preferable a form element */
+  Container: React.ReactElement<HTMLDivElement> | React.ForwardRefExoticComponent<any | React.RefAttributes<HTMLDivElement>>
+
+  // ** The children of the form */
+  children?: React.ReactNode
+
   // ** The recaptcha site key */
   recaptcha_site_key: string
 
@@ -50,7 +55,7 @@ export interface ISharedFormProps {
   debug?: boolean
 }
 
-export const SharedForm = forwardRef<HTMLDivElement, ISharedFormProps & React.HTMLAttributes<HTMLDivElement>>((props, ref) => {
+export const SharedForm = forwardRef<HTMLDivElement, ISharedFormProps & ISharedFormWordpressData & React.HTMLAttributes<HTMLDivElement>>((props, ref) => {
   const {
     // Children
     children,
@@ -63,6 +68,20 @@ export const SharedForm = forwardRef<HTMLDivElement, ISharedFormProps & React.HT
     // HTML Props and Container
     Container = 'div', ...rest
   } = props;
+
+  if (debug && !recaptcha_site_key) throw new Error('You need to provide a recaptcha site key');
+  if (debug && !mailSender) throw new Error('You need to provide a mail sender');
+  if (debug && !mailTransport) throw new Error('You need to provide a mail transport');
+  if (debug && !senderEnvelope) throw new Error('You need to provide a sender envelope');
+  if (debug && !mailReceiver) throw new Error('You need to provide a mail receiver');
+  if (debug && !confirmationSubject) throw new Error('You need to provide a confirmation subject');
+  if (debug && !confirmationPreviewText) throw new Error('You need to provide a confirmation preview text');
+  if (debug && !confirmationEmail) throw new Error('You need to provide a confirmation email');
+  if (debug && !dataReceiverEmail) throw new Error('You need to provide a data receiver email');
+  if (debug && !dataReceiverSubject) throw new Error('You need to provide a data receiver subject');
+  if (debug && !dataReceiverPreviewText) throw new Error('You need to provide a data receiver preview text');
+
+  if (debug && !children) throw new Error('You need to provide children inside the form');
 
   const CreatedContainer = createContainer(Container);
 
